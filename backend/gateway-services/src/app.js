218a41c711639fb7d7ad0aa1dceb6092fcbe13c3
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-06-22 16:29:53 +0530

    backend(gateway-services) : insert to aplmiter to app.js

--- backend/gateway-services/src/app.js
+++ backend/gateway-services/src/app.js
@@ -2,14 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
-
-
+import { apiLimiter } from './middleware/ratelimiter.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(apiLimiter);
 
 
 
