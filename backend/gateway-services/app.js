a266e9a2308d1c4f5a335ab301c9d27c896981bc
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-06-22 12:18:31 +0530

    backend(gateway-service(service.js)) : creatd service.js

--- backend/gateway-services/app.js
+++ backend/gateway-services/app.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 
-dotenv.config();
+
 
 const app = express();
 
@@ -11,10 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Gateway service running on port ${PORT}`);
-});
+
 
 export default app;
