a51f39b4c2573c408cf235f5ff89bd9aa5dcbe86
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-06-23 10:55:33 +0530

    backend(auth-service) : created login controller

--- backend/auth-service/src/controllers/authController.js
+++ backend/auth-service/src/controllers/authController.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcryptjs";
 import User from "../model/user.js";
+import jwt from "jsonwebtoken";
+import generateToken from "../utils/genarateToken.js";
 
 export const register = async (req, res) => {
     try {
@@ -46,6 +48,61 @@ export const register = async (req, res) => {
         res.status(201).json({
             success: true,
             message: "User registered successfully",
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Email and password are required"
+            });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials"
+            });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isPasswordMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials"
+            });
+        }
+
+        const token = generateToken(user._id);
+
+        res.status(200).json({
+            success: true,
+            message: "Login successful",
+            token,
             user: {
                 id: user._id,
                 username: user.username,
