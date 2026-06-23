19be5d7b19abea09f122779041d79396d4546b79
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-06-23 08:14:24 +0530

    backend(auth-service) : create user registration function

---
+++ backend/auth-service/src/controllers/authController.js
@@ -0,0 +1,64 @@
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+
+export const register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            });
+        }
+
+       
+        const existingEmail = await User.findOne({ email });
+
+        if (existingEmail) {
+            return res.status(400).json({
+                success: false,
+                message: "Email already exists"
+            });
+        }
+
+    
+        const existingUsername = await User.findOne({ username });
+
+        if (existingUsername) {
+            return res.status(400).json({
+                success: false,
+                message: "Username already exists"
+            });
+        }
+
+    
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+       
+        const user = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+};
