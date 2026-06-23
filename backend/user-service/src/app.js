6bc3fe6f9207c00bb2e84e2a75581e6af64a7bbf
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-06-23 17:17:06 +0530

    backend(user-service) : created user and server also .env file

---
+++ backend/user-service/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.json({
+        service: "User Service",
+        status: "Running"
+    });
+});
+
+export default app;
