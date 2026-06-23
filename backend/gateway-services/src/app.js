041c6122b72e01e0f6428955edc16a3780821fa6
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-06-23 12:19:15 +0530

    backend(gateway-service) : add authMiddleware and update app.js

--- backend/gateway-services/src/app.js
+++ backend/gateway-services/src/app.js
@@ -22,6 +22,19 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get(
+    "/api/protected",
+    verifyToken,
+    (req, res) => {
+
+        res.status(200).json({
+            success: true,
+            message: "Protected Route Accessed",
+            user: req.user
+        });
+
+    }
+);
 
 
 export default app;
