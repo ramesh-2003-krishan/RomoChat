572d4ae3f5a9ee4c9d2b81d0b527bde2f2c33373
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-06-28 12:16:30 +0530

    backend(chat-service) : register chat service

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/", chatRoutes);
 
 app.get("/", (req, res) => {
 res.json({
